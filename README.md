84ae0584264092c25296b103d1830f6c6931b1ac
Mauricio Chirino <mauricio@3codegeeks.com>
2017-07-24 14:01:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# UdacityMap
+On the map project for Udacity's nanodegree iOS course
+
+Project built with vanilla Swift 3 (no `pods` for handling anything you see) due to requiriments below.
+
+https://docs.google.com/document/d/1tPF1tmSzVYPSbpl7_JCeMKglKMIs3dUa4OrSAKEYNAs/pub?embedded=true
+
+Requirements: Xcode 8.3.3+ and internet connection.
---
+++ UdacityMap/UdacityMap/CredentialsController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UdacityMap
+//
+//  Created by Mauricio Chirino on 22/7/17.
+//  Copyright © 2017 3CodeGeeks. All rights reserved.
+//
+
+import UIKit
+
+class CredentialsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
